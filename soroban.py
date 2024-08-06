877f3ec8eb954c0b1219e7155ee8f7d3fe01cf25
Christian Isaman <christianisaman@outlook.com>
2024-08-06 11:49:46 +0800

    add subtraction, hundred-thousands

---
+++ soroban.py
@@ -0,0 +1,83 @@
+def init_rods():
+    ones = [0 for i in range(5)]
+    tens = [0 for i in range(5)]
+    hundreds = [0 for i in range(5)]
+    thousands = [0 for i in range(5)]
+    ten_thousands = [0 for i in range(5)]
+    hundred_thousands = [0 for i in range(5)]
+    return hundred_thousands, ten_thousands, thousands, hundreds, tens, ones
+
+
+def set_rods(value, rods):
+    (
+        hundred_thousands_rod,
+        ten_thousands_rod,
+        thousands_rod,
+        hundreds_rod,
+        tens_rod,
+        ones_rod,
+    ) = rods
+    hundred_thousands_digit = (value // 100000) % 10
+    ten_thousands_digit = (value // 10000) % 10
+    thousands_digit = (value // 1000) % 10
+    hundreds_digit = (value // 100) % 10
+    tens_digit = (value // 10) % 10
+    ones_digit = value % 10
+    if ones_digit >= 5:
+        ones_rod[0] = 1
+        ones_digit -= 5
+    if tens_digit >= 5:
+        tens_rod[0] = 1
+        tens_digit -= 5
+    if hundreds_digit >= 5:
+        hundreds_rod[0] = 1
+        hundreds_digit -= 5
+    if thousands_digit >= 5:
+        thousands_rod[0] = 1
+        thousands_digit -= 5
+    if ten_thousands_digit >= 5:
+        ten_thousands_rod[0] = 1
+        ten_thousands_digit -= 5
+    if hundred_thousands_digit >= 5:
+        hundred_thousands_rod[0] = 1
+        hundred_thousands_digit -= 5
+    for i in range(1, ones_digit + 1):
+        ones_rod[i] = 1
+    for i in range(1, tens_digit + 1):
+        tens_rod[i] = 1
+    for i in range(1, hundreds_digit + 1):
+        hundreds_rod[i] = 1
+    for i in range(1, thousands_digit + 1):
+        thousands_rod[i] = 1
+    for i in range(1, ten_thousands_digit + 1):
+        ten_thousands_rod[i] = 1
+    for i in range(1, hundred_thousands_digit + 1):
+        hundred_thousands_rod[i] = 1
+    return
+
+
+def print_soroban(rods):
+    (
+        hundred_thousands_rod,
+        ten_thousands_rod,
+        thousands_rod,
+        hundreds_rod,
+        tens_rod,
+        ones_rod,
+    ) = rods
+    print("●", end="") if (hundred_thousands_rod[0]) else print(" ", end="")
+    print("●", end="") if (ten_thousands_rod[0]) else print(" ", end="")
+    print("●", end="") if (thousands_rod[0]) else print(" ", end="")
+    print("●", end="") if (hundreds_rod[0]) else print(" ", end="")
+    print("●", end="") if (tens_rod[0]) else print(" ", end="")
+    print("●", end="") if (ones_rod[0]) else print(" ", end="")
+    print("\n––––––")
+    for i in range(1, 5):
+        print("●", end="") if (hundred_thousands_rod[i]) else print(" ", end="")
+        print("●", end="") if (ten_thousands_rod[i]) else print(" ", end="")
+        print("●", end="") if (thousands_rod[i]) else print(" ", end="")
+        print("●", end="") if (hundreds_rod[i]) else print(" ", end="")
+        print("●", end="") if (tens_rod[i]) else print(" ", end="")
+        print("●", end="") if (ones_rod[i]) else print(" ", end="")
+        print()
+    print()
--- subtract.py
+++ subtract.py
@@ -5,11 +5,11 @@ from soroban import init_rods, print_soroban, set_rods
 if __name__ == "__main__":
     print(
         "How many digits do you want to add?",
-        "Enter 1 (eg. 7 + 8), or",
-        "Enter 2 (eg. 47 + 12), or",
-        "Enter 3 (eg. 347 + 112), or",
-        "Enter 4 (eg. 2347 + 6194), or",
-        "Enter 5 (eg. 59187 + 19112)",
+        "Enter 1 (eg. 9 - 3), or",
+        "Enter 2 (eg. 47 - 12), or",
+        "Enter 3 (eg. 347 - 112), or",
+        "Enter 4 (eg. 6194 - 2347), or",
+        "Enter 5 (eg. 59187 - 19112)",
         sep="\n",
     )
     digits = int(input())
@@ -17,13 +17,13 @@ if __name__ == "__main__":
     while True:
         rods = init_rods()
         max = 10 ** (digits) - 1
-        x = randint(1, max)
-        y = randint(1, max)
-        print(f"{x} + {y} = ?")
+        x = randint(1, max - 1)
+        y = randint(x + 1, max)
+        print(f"{y} - {x} = ?")
         re = input()
         if re == "q":
             break
-        sum = x + y
-        print(sum)
-        set_rods(sum, rods)
+        difference = y - x
+        print(difference)
+        set_rods(difference, rods)
         print_soroban(rods)
